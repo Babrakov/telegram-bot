e4ae788dd25bb4b4a9254e714998dfbafac94997
Sergei Babrakov <s.a.babrakov@gmail.com>
2023-01-29 19:51:48 +0100

    Files from spring initializer

---
+++ src/main/java/ru/infoza/telegrambot/TelegramBotApplication.java
@@ -0,0 +1,15 @@
+package ru.infoza.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+//@SpringBootApplication
+public class TelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/infoza/telegrambot/TelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.infoza.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
